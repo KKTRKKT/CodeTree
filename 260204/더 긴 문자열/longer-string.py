3fbc4b22e470c70d3f7622371301478c644465ed
Lee Sung Hyun <paulhana6006@gmail.com>
2026-02-04 16:52:18 +0900

    Title: 더 긴 문자열, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/더 긴 문자열/longer-string.py
+++ 260204/더 긴 문자열/longer-string.py
@@ -1,3 +1,3 @@
 str1, str2 = input().split()
-long_str = str1 if len(str1) > len(str2) else str2
-print(long_str, len(long_str))
+long_str = str1 if len(str1) > len(str2) else str2 if len(str2) > len(str1) else 'same'
+print(*[long_str, len(long_str)] if long_str != 'same' else [long_str])
