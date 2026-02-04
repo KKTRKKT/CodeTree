10f40ec116d899f70d2fd83505fbc73044334a96
Lee Sung Hyun <paulhana6006@gmail.com>
2026-02-04 17:00:39 +0900

    Title: 100점 도달하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/100점 도달하기/reach-100-point.py
+++ 260204/100점 도달하기/reach-100-point.py
@@ -1,4 +1,4 @@
 score = int(input())
-for i in range(score, 100):
+for i in range(score, 101):
     print('A' if score >= 90 else 'B' if score >= 80 else 'C' if score >= 70 else 'D' if score >= 60 else 'F', end=" ")
     score += 1
